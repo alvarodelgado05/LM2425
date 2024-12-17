aefd3fe746d9331c21e2cd93927ef59d035ea2af
Alvaro Delgado <alvarodelgado05@iesamachado.org>
2024-12-17 14:22:49 +0100

    .

---
+++ html/t3-java/ejercicios/circulos.js
@@ -0,0 +1,17 @@
+/*
+Este fichero contiene el código
+para ocultar un círculo
+*/
+function ocultar(id) {
+    // Esta función permite ocultar un círculo
+    document.getElementById(id).style.visibility = 'hidden';
+    document.getElementById(id).style.visibility = 'hidden';
+    document.getElementById(id).style.visibility = 'hidden';
+}
+
+function mostrar(id) {
+    // Esta función permite mostrar un círculo
+    document.getElementById(id).style.visibility = 'visible';
+    document.getElementById(id).style.visibility = 'visible';
+    document.getElementById(id).style.visibility = 'visible';
+}
