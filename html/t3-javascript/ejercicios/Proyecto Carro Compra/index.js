4c05eda3236cabd4f08c99662967cc903c345588
Alvaro Delgado <alvarodelgado05@iesamachado.org>
2025-04-08 12:07:45 +0200

    .

---
+++ html/t3-javascript/ejercicios/Proyecto Carro Compra/index.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Jamón Ibérico",
+        descripcion: "Jamón de bellota curado de forma tradicional.",
+        precio: 25.00,
+        imagen: "Jamón_bellota_Muñana.jpeg"
+    },
+    {
+        id: 2,
+        nombre: "Chorizo Ibérico",
+        descripcion: "Chorizo ibérico con pimentón de la Vera.",
+        precio: 8.50,
+        imagen: "chor_iberi.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Salchichón Ibérico",
+        descripcion: "Salchichón curado con especias naturales.",
+        precio: 7.90,
+        imagen: "salchichon-iberico-condimento-1.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Lomo Embuchado",
+        descripcion: "Lomo de cerdo ibérico embuchado.",
+        precio: 12.00,
+        imagen: "lomo_embu.jpg"
+    }
+];
